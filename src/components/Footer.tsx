4bd714034724e93e37df4a0b0648662a35d4846e
Sam Mosios <sam.mosios@gmail.com>
2026-01-11 17:25:03 +0100

    chore: Dynamically reads name in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import './Footer.css';
+import { profile } from '@/user';
 
 const Footer = () => {
   return (
     <footer className='app-footer'>
-      <p>&copy; 2026 Your Name. All rights reserved.</p>
+      <p>&copy; 2026 {profile.name}. All rights reserved.</p>
     </footer>
   );
 };
